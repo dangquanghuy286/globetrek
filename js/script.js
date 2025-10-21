6b118b03f8390e54a7e1ed41443ef25b84313074
dangquanghuy286 <154116366+dangquanghuy286@users.noreply.github.com>
2025-10-21 11:24:58 +0700

    up

--- js/script.js
+++ js/script.js
@@ -32,3 +32,44 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 });
+// =============BreadCrumb==================
+document.addEventListener("DOMContentLoaded", () => {
+  const breadcrumbContainer = document.getElementById("breadcrumb");
+  if (!breadcrumbContainer) return;
+
+  // Lay dia chi path ,chia chuoi va loc cac phan tu rong
+  const path = window.location.pathname;
+  const parts = path.split("/").filter(Boolean);
+
+  // Hàm viết hoa chữ cái đầu tiên
+  const capitalizeWords = (str) => {
+    return str
+      .split(" ")
+      .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+      .join(" ");
+  };
+
+  // path home
+  let breadcrumbHTML = `
+    <a class="breadcrumb-item" href="index.html">Home</a>
+  `;
+
+  parts.forEach((part, index) => {
+    const isLast = index === parts.length - 1;
+    const name = capitalizeWords(part.replace(".html", "").replace(/-/g, " "));
+
+    if (!isLast) {
+      breadcrumbHTML += `
+        <div class="breadcrumb-item dot"><span> > </span></div>
+        <a class="breadcrumb-item" href="${part}">${name}</a>
+      `;
+    } else {
+      breadcrumbHTML += `
+        <div class="breadcrumb-item dot"><span> > </span></div>
+        <div class="breadcrumb-item current">${name}</div>
+      `;
+    }
+  });
+
+  breadcrumbContainer.innerHTML = breadcrumbHTML;
+});
