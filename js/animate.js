8c24549394f0716e8b2ac9684d40a4ab9cdc40a6
dangquanghuy286 <154116366+dangquanghuy286@users.noreply.github.com>
2025-11-12 19:01:31 +0700

    up

--- js/animate.js
+++ js/animate.js
@@ -1,10 +1,6 @@
-// =============Preload================
-window.addEventListener("load", function () {
+document.addEventListener("DOMContentLoaded", function () {
   const preload = document.querySelector(".preload");
-
-  setTimeout(() => {
-    preload.style.transition = "opacity 1s ease";
-    preload.style.opacity = "0";
-    setTimeout(() => preload.remove(), 500);
-  }, 1000);
+  preload.style.transition = "opacity 1s ease";
+  preload.style.opacity = "0";
+  setTimeout(() => preload.remove(), 1200);
 });
