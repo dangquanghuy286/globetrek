d64cb457b1d70bf2d98e7a136589b2d83edc1281
dangquanghuy286 <154116366+dangquanghuy286@users.noreply.github.com>
2025-11-13 23:15:15 +0700

    up

--- js/swiper.js
+++ js/swiper.js
@@ -181,3 +181,45 @@ document.addEventListener("DOMContentLoaded", function () {
     window.addEventListener("resize", initSwiperMb);
   }
 });
+// ===================Find Wish========================
+document.addEventListener("DOMContentLoaded", () => {
+  const tfSwLatest = document.querySelector(".tf-perfect-tour");
+  if (tfSwLatest) {
+    const preview = tfSwLatest.dataset.preview;
+    const tablet = tfSwLatest.dataset.tablet;
+    const mobile = tfSwLatest.dataset.mobile;
+    const mobileSm = tfSwLatest.dataset.mobileSm;
+    const spacingLg = tfSwLatest.dataset.spaceLg;
+    const spacingMd = tfSwLatest.dataset.spaceMd;
+    const spacing = tfSwLatest.dataset.space;
+
+    const swiper = new Swiper(".tf-perfect-tour", {
+      slidesPerView: parseInt(mobile),
+      spaceBetween: parseInt(spacing),
+      pagination: {
+        el: ".sw-pagination-wish",
+        clickable: true,
+      },
+      navigation: {
+        clickable: true,
+        nextEl: ".nav-prev-latest",
+        prevEl: ".nav-next-latest",
+      },
+      breakpoints: {
+        575: {
+          slidesPerView: parseInt(mobileSm),
+          spaceBetween: parseInt(spacing),
+        },
+        768: {
+          slidesPerView: parseInt(tablet),
+          spaceBetween: parseInt(spacingMd),
+        },
+        1200: {
+          slidesPerView: parseInt(preview),
+          spaceBetween: parseInt(spacingLg),
+        },
+      },
+    });
+  }
+});
+// ==
